3cd9ed18e6521731d9cbb2d648cea2b193d14225
khushali08798 <khushalichawrekar@gmail.com>
2025-04-01 16:03:16 +0530

    forward done add previous

---
+++ Testing/src/Test_Iterator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Test_Iterator {
+	
+	public static void main(String[] args)
+	{
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("KH");
+		list.add("US");
+		list.add("HA");
+		list.add("LI");
+		
+		ListIterator<String> litr= list.listIterator();
+		
+		while(litr.hasNext())
+		{
+			System.out.print(litr.next());
+		}
+	
+	}
+
+}
